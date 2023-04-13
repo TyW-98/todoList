1553079f703077db848f3e44f104faa7887fd5ea
wey1998 <yitwey1998@hotmail.com>
2023-04-14 00:32:16 +0100

    Use Date method to add date and day to ejs template

--- v1/script/app.js
+++ v1/script/app.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const https = require('https');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const https = require("https");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Test js file ')
-})
+const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',]
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const todayDate = `${new Date().getDate()}/${
+    new Date().getMonth() + 1
+  }/${new Date().getFullYear()}`;
+  const dayOfWeek = week[new Date().getDay()]
+
+  res.render('list', {
+    todayDate: todayDate,
+    dayOfWeek: dayOfWeek
+  })
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
+  console.log("Server is running on port 3000");
+});
