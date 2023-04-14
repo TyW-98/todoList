e1efad6a5e8700989c6e25caf3954353fe66dbe5
wey1998 <yitwey1998@hotmail.com>
2023-04-14 01:01:29 +0100

    Add input box and button to allow users to enter new task

--- v1/script/app.js
+++ v1/script/app.js
@@ -5,18 +5,24 @@ const https = require("https");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const dateOptions = {
   day: "numeric",
   month: "short",
   year: "numeric",
   weekday: "long",
 };
-const fullDate = new Date().toLocaleDateString("en-us", dateOptions);
 
-app.set("view engine", "ejs");
+const fullDate = new Date().toLocaleDateString("en-us", dateOptions);
 
 app.get("/", (req, res) => {
   res.render("list", { todayFullDate: fullDate });
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body.newTask);
 });
 
 app.listen(3000, () => {
