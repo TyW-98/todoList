c45eb65cc77cd7457b3d0c468286ab2c546e0d1a
wey1998 <yitwey1998@hotmail.com>
2023-04-21 23:20:20 +0100

    Connect server with home page and fetch data from server and render it

--- script/app.js
+++ script/app.js
@@ -5,52 +5,75 @@ const https = require("https");
 const date = require(path.join(__dirname, "todaysDate.js"));
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/todoListDB")
-
-const taskSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Need to enter task name"]
-  }
-});
-
-const Task = mongoose.model("Task", taskSchema);
-
 const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/todoListDB");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const taskSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Need to enter task name"],
+  },
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+const task1 = new Task({
+  name: "task1",
+});
+
+const task2 = new Task({
+  name: "task2",
+});
+
+const task3 = new Task({
+  name: "task3",
+});
+
+const defaultItems = [task1, task2, task3];
+
+// Task.insertMany(defaultItems)
+//   .then(() => {
+//     console.log("Successfully added task to database");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
 const fullDate = date.getTodaysDate();
 
 app.get("/", (req, res) => {
+
   let listType = "Home";
-  res.render("list", {
-    listType: listType,
-    todayFullDate: fullDate,
-    taskList: homeTaskList,
-  });
+
+  Task.find().then (task => {
+    res.render("list", {listType: listType, todayFullDate: fullDate, taskList: task})
+  })
+
 });
 
-app.post("/", (req, res) => {
-  if (req.body.submitBtn === "Home") {
-    homeTaskList.push(req.body.newTask);
-    res.redirect("/");
-  } else {
-    workTaskList.push(req.body.newTask);
-    res.redirect("/work");
-  }
-});
+// app.post("/", (req, res) => {
+//   if (req.body.submitBtn === "Home") {
+//     homeTaskList.push(req.body.newTask);
+//     res.redirect("/");
+//   } else {
+//     workTaskList.push(req.body.newTask);
+//     res.redirect("/work");
+//   }
+// });
 
-app.get("/work", (req, res) => {
-  let listType = "Work";
-  res.render("list", {
-    listType: listType,
-    todayFullDate: fullDate,
-    taskList: workTaskList,
-  });
-});
+// app.get("/work", (req, res) => {
+//   let listType = "Work";
+//   res.render("list", {
+//     listType: listType,
+//     todayFullDate: fullDate,
+//     taskList: workTaskList,
+//   });
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
